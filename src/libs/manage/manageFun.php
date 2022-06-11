52481c5126fbff3cc9e0ad33be11d5e3a5b986b9
Salman <50276554+MrHooRa@users.noreply.github.com>
2022-06-11 14:56:06 +0300

    Create manageFun.php

---
+++ src/libs/manage/manageFun.php
@@ -0,0 +1,16 @@
+<?php
+
+//==================================================
+/**
+ * This file contain general functions for use in manage page.
+ * 
+ * Priority: 2
+ * 
+ * Used in:
+ *      -
+ * 
+ * Need to:
+ *      ./config/config.php             Priority: 0
+ *      ./src/lib/database.php          Priority: 1
+ */
+//==================================================
