d242d1c98dd4312e574832f75f54398bcfcb6d9d
Salman <50276554+MrHooRa@users.noreply.github.com>
2022-06-17 18:17:57 +0300

    Fix search bar

--- manage/subjects.php
+++ manage/subjects.php
@@ -57,10 +57,18 @@ getPageHead(3, "لوحة التحكم");
                         </div>
 
                      </div>
+
                      <div class="ms-auto text-muted">
-                        بحث:
-                        <div class="ms-2 d-inline-block">
-                           <input type="text" id="search_input" class="form-control form-control-sm" aria-label="Search subject" onkeyup="load_data(this.value);">
+                        <div class="input-icon">
+                           <input type="text" id="search_input" class="form-control form-control-rounded" aria-label="Search subject" placeholder="بحث..." onkeyup="load_data(this.value);">
+                           <span class="input-icon-addon">
+                              <!-- Download SVG icon from http://tabler-icons.io/i/search -->
+                              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                 <circle cx="10" cy="10" r="7"></circle>
+                                 <line x1="21" y1="21" x2="15" y2="15"></line>
+                              </svg>
+                           </span>
                         </div>
                      </div>
                   </div>
